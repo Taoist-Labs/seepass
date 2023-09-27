4c314dad2d8ce92083cc17c037c87637b238c924
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-09-27 10:54:06 +0800

    change sns package

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,6 +1,4 @@
-import { PublicResolver } from "sns-contracts/lib/contracts/resolvers";
-import { PublicResolverContract } from "sns-contracts";
-import { ethers } from "ethers";
+import { sns } from "@seedao/sns-js";
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
@@ -8,7 +6,6 @@ export default function Profile() {
   const { domain } = useParams();
   const navigate = useNavigate();
 
-  const [contract, setContract] = useState<PublicResolver>();
   const [wallet, setWallet] = useState("");
 
   if (!domain) {
@@ -16,23 +13,10 @@ export default function Profile() {
   }
 
   useEffect(() => {
-    const initContract = async () => {
-      const provider = new ethers.JsonRpcProvider(
-        "https://ethereum-sepolia.blockpi.network/v1/rpc/public"
-      );
-      const _contract: PublicResolver =
-        PublicResolverContract.atSepolia().connect(provider);
-      setContract(_contract);
-    };
-    initContract();
-  }, []);
-
-  useEffect(() => {
-    console.log("domain: ", domain);
-    console.log("contract: ", contract);
     const parseDomain = async () => {
-      if (contract && domain) {
-        contract["addr(bytes32)"](ethers.namehash(domain))
+      domain &&
+        sns
+          .resolve(domain)
           .then((addr) => {
             console.log("addr:", addr);
             setWallet(addr);
@@ -41,11 +25,10 @@ export default function Profile() {
             console.error("parse domain failed", error);
             navigate("/404");
           });
-      }
     };
-
     parseDomain();
-  }, [contract, domain, navigate]);
+  }, [domain, navigate]);
+
   return (
     <div className="Profile">
       <p>SeePASS</p>

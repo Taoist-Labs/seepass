aa8e6f88b13d67b8dccb604006a3bd47d1ad1646
wendychaung <7518647+wendychaung@users.noreply.github.com>
2023-11-13 11:57:33 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -2,5 +2,5 @@ window.config={
     // dev
     // BASEURL:"https://test-seepass-api.seedao.tech"
 
-    BASEURL:"https://seepass-api.seedao.tech"
+    BASEURL:"https://test-seepass-api.seedao.tech"
 }
